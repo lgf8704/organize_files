1ddf95944242c6826352911d582663f7bb858258
lgf8704 <648008748@qq.com>
2020-05-25 15:34:52 +0800

    1

--- delete_folders.py
+++ delete_folders.py
@@ -9,3 +9,4 @@ def delete_folders(path='.'):
         if len(folder) > 20:
             folder_path = os.path.join(path, folder)
             shutil.rmtree(folder_path)
+            print(f"正在删除{folder_path}文件夹")
